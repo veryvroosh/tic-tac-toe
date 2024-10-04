f69dcfb62fd0729ecd9eb39ed5da69d81c653b9c
vroosh <zaidsafadi.2005@gmail.com>
2024-10-04 20:01:03 +0300

    finished functionality, and basic design excluding footer

--- tictactoe.js
+++ tictactoe.js
@@ -1,27 +1,36 @@
-function createPlayer (name, marker) {
-    const playerName = name;
-    const playerMarker = marker;
-
-    return { playerName , playerMarker};
-}
 
 const game = (function() {
     let gameBoard = [];
     let roundIndicator = 1;
 
-    return {
-        playRound: function() {
-            let placement = prompt("Where do you want to place your marker?");
+    const cells = document.querySelectorAll(".game-cell");
+
+    cells.forEach((cell) => {
+        cell.addEventListener('click', () => {
+
+            let placement = Number.parseInt(cell.name);
             if (roundIndicator%2 === 1) {
                 gameBoard[placement-1] = "X";
+                cell.textContent = "X";
+                if(game.checkWin()===true) {
+                    console.log("Player 1 Won");
+                }
             } else {
                 gameBoard[placement-1] = "O";
+                cell.textContent = "O";
+                if(game.checkWin()===true) {
+                    console.log("Player 2 Won");
+                }
             }
+
+            cell.disabled = true;
 
             game.displayBoard();
             roundIndicator++;
-        },
+        });
+    });
 
+    return {
         checkWin: function () {
             const winningPatterns = [
                 [0, 1, 2],
@@ -36,17 +45,17 @@ const game = (function() {
 
             const winningPattern = (pattern) => {
                 let [a, b, c] = pattern;
-                return gameBoard[a] !== null && gameBoard[a] === gameBoard[b] && gameBoard[b] === gameBoard[c];
+                return (gameBoard[a] !== undefined && gameBoard[a] === gameBoard[b] && gameBoard[b] === gameBoard[c]);
             };
 
             return winningPatterns.some(winningPattern);
         },
 
-        displayBoard: function () {
+      /*  displayBoard: function () {
             console.log(gameBoard[0], gameBoard[1], gameBoard[2]);
             console.log(gameBoard[3], gameBoard[4], gameBoard[5]);
             console.log(gameBoard[6], gameBoard[7], gameBoard[8]);
-        }
+        }*/
     }
 })();
 
