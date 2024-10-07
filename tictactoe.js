37b24222acbf0f5197888bc5a3f47ef885db57e0
vroosh <zaidsafadi.2005@gmail.com>
2024-10-07 11:04:03 +0300

    game now repeats with a 3s delay

--- tictactoe.js
+++ tictactoe.js
@@ -1,36 +1,41 @@
-
 const game = (function() {
+
     let gameBoard = [];
-    let roundIndicator = 1;
+    let roundIndicator = 0;
+    let eventListeners = [];
 
     const cells = document.querySelectorAll(".game-cell");
 
-    cells.forEach((cell) => {
-        cell.addEventListener('click', () => {
+    function handleCellClick(cell) {
+        cell.disabled = true;
+        roundIndicator++;
+        console.log(roundIndicator);
 
-            let placement = Number.parseInt(cell.name);
-            if (roundIndicator%2 === 1) {
-                gameBoard[placement-1] = "X";
-                cell.textContent = "X";
-                if(game.checkWin()===true) {
-                    console.log("Player 1 Won");
-                }
-            } else {
-                gameBoard[placement-1] = "O";
-                cell.textContent = "O";
-                if(game.checkWin()===true) {
-                    console.log("Player 2 Won");
-                }
+        let placement = Number.parseInt(cell.name);
+        if (roundIndicator%2 === 1) {
+            gameBoard[placement-1] = "X";
+            cell.textContent = "X";
+            if (game.checkWin() === true) {
+                game.roundWon("Player1")
             }
-
-            cell.disabled = true;
-
-            game.displayBoard();
-            roundIndicator++;
-        });
-    });
+        } else {
+            gameBoard[placement-1] = "O";
+            cell.textContent = "O";
+            if(game.checkWin()===true) {
+                game.roundWon("Player2")
+            }
+        }
+    }
 
     return {
+        playRound: function () {
+            cells.forEach((cell) => {
+                const listener = () => handleCellClick(cell);
+                cell.addEventListener('click', listener);
+                eventListeners.push({ cell, listener });
+            });
+        },
+
         checkWin: function () {
             const winningPatterns = [
                 [0, 1, 2],
@@ -51,12 +56,42 @@ const game = (function() {
             return winningPatterns.some(winningPattern);
         },
 
-      /*  displayBoard: function () {
+        roundWon: function(winnerName) {
+            console.log(`${winnerName} won the game!`);
+            game.removeEventListeners();
+            setTimeout(startGame, 3000);
+        },
+
+        cleanGame: function() {
+            roundIndicator = 0;
+            for(let i=0; i<9; i++) {
+                gameBoard[i] = undefined;
+            }
+
+            cells.forEach(cell => {
+                cell.textContent = "";
+                cell.disabled = false;
+            });
+        },
+
+        removeEventListeners: function () {
+            eventListeners.forEach(({ cell, listener }) => {
+                cell.removeEventListener('click', listener);
+            });
+            eventListeners = [];
+        },
+
+        displayBoard: function () {
             console.log(gameBoard[0], gameBoard[1], gameBoard[2]);
             console.log(gameBoard[3], gameBoard[4], gameBoard[5]);
             console.log(gameBoard[6], gameBoard[7], gameBoard[8]);
-        }*/
-    }
+        }
+    };
 })();
 
+function startGame () {
+    game.cleanGame();
+    game.playRound();
+}
 
+startGame();
